2a45088db2efaa3483e2def15320f8166529d957
Phidung <dungho01092002@gmail.com>
2022-05-10 22:11:51 +0700

    fix venv

--- flaskr/static/js/login.js
+++ flaskr/static/js/login.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { WalletLink } = require("walletlink");
+
 /**
  * Example JavaScript code that interacts with the page and Web3 wallets
  */
@@ -7,7 +9,6 @@
 // Unpkg imports
 const ethers = window.ethers; // for signing.
 const Web3Modal = window.Web3Modal.default;
-const evmChains = window.evmChains;
 
 // Web3modal instance
 let web3Modal;
@@ -17,6 +18,9 @@ let provider;
 
 // Address of the selected account
 let selectedAccount;
+let web3;
+
+const INFURA_ID = "<YOUR_INFURA_ID>";
 
 /**
  * Setup the orchestra
@@ -31,12 +35,26 @@ function init() {
   );
 
   // Tell Web3modal to add other wallets besides Metamask, if any.
-  const providerOptions = {};
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        infuraId: INFURA_ID,
+      },
+    },
+    walletlink: {
+      package: WalletLink,
+      options: {
+        infuraId: INFURA_ID,
+      },
+    },
+  };
 
   web3Modal = new Web3Modal({
     cacheProvider: false, // optional
     providerOptions, // required
     disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
+    theme: "dark",
   });
 
   console.log("Web3Modal instance is", web3Modal);
@@ -68,7 +86,7 @@ async function signMetaMask(message, account) {
 }
 
 /**
- * Verify a message using the signature. Should this be done on python backend?
+ * Verify a message using the signature. This should be done on python backend
  */
 async function verifyMessage(message, signature, address) {
   try {
@@ -77,7 +95,7 @@ async function verifyMessage(message, signature, address) {
       .toLowerCase();
     console.log("Signer address:", signerAddr);
     console.log("Address:", address);
-    return signerAddr === address;
+    return signerAddr == address;
   } catch (err) {
     console.log(err);
     return false;
@@ -85,26 +103,25 @@ async function verifyMessage(message, signature, address) {
 }
 
 /**
- * Connect using MetaMask API.
- */
-async function onConnect() {
-  const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-  selectedAccount = accounts[0];
-  // Generate a random message (the nonce).
-  const message = self.crypto.randomUUID();
-  // Sign the message.
-  let signature = await signMetaMask(message, selectedAccount);
-  // TODO: make a POST request to the backend.
-}
-/**
  * Connect using web3Modal API.
  * @returns a POST request contain account and signature validity.
  */
 async function onConnectWeb3Modal() {
   console.log("Opening a dialog", web3Modal);
   try {
-    // source code: https://github.com/Web3Modal/web3modal/blob/master/src/core/index.tsx#L71
     provider = await web3Modal.connect();
+    await provider.enable();
+
+    web3 = new Web3(provider);
+    web3.eth.extend({
+      methods: [
+        {
+          name: "chainId",
+          call: "eth_chainId",
+          outputFormatter: web3.utils.hexToNumber,
+        },
+      ],
+    });
   } catch (e) {
     console.log("Could not get a wallet connection", e);
     return;
@@ -112,17 +129,15 @@ async function onConnectWeb3Modal() {
   // This is a reference of window.ethereum.
   console.log("Got a wallet connection", provider);
   // Get list of accounts of the connected wallet
-  const accounts = await provider.request({ method: "eth_requestAccounts" });
+  const accounts = await web3.eth.getAccounts();
   // MetaMask does not give you all accounts, only the selected account
   console.log("Got accounts", accounts);
   selectedAccount = accounts[0];
 
   // Generate a message padded with nonce.
   const message = String(
-    "I agree to login on this page." +
-      "\nLogin time: " +
+    "I agree to login on this page.\nLogin time: " +
       new Date().toLocaleDateString("en-US")
-    // self.crypto.randomUUID().toString()
   );
   console.log("Message is: ", message);
 
@@ -137,7 +152,6 @@ async function onConnectWeb3Modal() {
       signature: signature,
     },
     (response) => {
-      console.log("Response from server:\n", response);
       // to redirect the web-page, can't be done from python.
       // Solution is found here but I don't understand yet:
       // https://stackoverflow.com/questions/38926335/flask-redirecturl-for-returning-html-but-not-loading-page/38926522#comment65211259_38926522
@@ -152,15 +166,8 @@ async function onConnectWeb3Modal() {
  */
 async function onDisconnect() {
   console.log("Killing the wallet connection", provider);
-
-  // TODO: Which providers have close method?
   if (!!provider && provider.close) {
     await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
     await web3Modal.clearCachedProvider();
     provider = null;
     // window.location.reload(true); // trigger reload to refresh the page information
@@ -173,7 +180,6 @@ async function onDisconnect() {
       logout: true,
     },
     (response) => {
-      console.log("Response from server:\n", response);
       window.location = response;
     }
   );
